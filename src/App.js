60833f6cc989a998bdd05e2d5e33506936a4ee09
ma-91-02 <ma.91.02@mail.ru>
2022-04-13 22:05:30 +0300

    uplode ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import logo from "./logo.svg";
-import Header from'./components/Header';
+import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ExpenseItem from "./components/ExpenseItem";
 import "./App.css";
 
 function App() {
-  return (<div><Header></Header><Footer></Footer></div>);
+  const expenses = [
+    {
+      title:'Car Insurance',amount:294.67, date: new Date(2021, 2, 28)
+    },
+    {
+      title:'Car Insurance',amount:294.67, date: new Date(2021, 2, 28)
+    },
+    {
+      title:'Car Insurance',amount:294.67, date: new Date(2021, 2, 28)
+    },
+    {
+      title:'Car Insurance',amount:294.67, date: new Date(2021, 2, 28)
+    },
+    {
+      title:'Car Insurance',amount:294.67, date: new Date(2021, 2, 28)
+    },
+  ];
+  return (
+    <div>
+      <Header></Header>
+      <ExpenseItem></ExpenseItem>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
