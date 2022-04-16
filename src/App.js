55ccc5009d52f5d506abeea22b066d30ac960f5d
ma-91-02 <ma.91.02@mail.ru>
2022-04-16 14:25:43 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses';
 import "./App.css";
 
 function App() {
@@ -34,31 +35,7 @@ function App() {
   return (
     <div>
       <Header></Header>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-            <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-            <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-            <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-            <ExpenseItem
-        title={expenses[4].title}
-        amount={expenses[4].amount}
-        date={expenses[4].date}
-      />
+      <Expenses items={expenses}/>
       <Footer></Footer>
     </div>
   );
